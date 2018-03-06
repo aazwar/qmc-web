945ff211085fb13c9e1a86c3da1c65be260d18a9
Azrul Azwar <azrul.azwar@gmail.com>
2018-03-06 09:50:36 +0700

    rating

--- app/controllers/ajax/rating.php
+++ app/controllers/ajax/rating.php
@@ -5,7 +5,7 @@ class Rating extends \Fuwafuwa\AjaxController {
   function rate() {
     $json = file_get_contents('php://input');
     $data = json_decode($json, true);
-    SQL('REPLACE INTO rating VALUES(?,?,?,?,?)', data.department_id, data.channel_id, date('Y-m-d\TH:i:s'), data.rating, data.review);
+    SQL('INSERT INTO rating VALUES(?,?,?,?,?)', $data['department_id'], $data['channel_id'], date('Y-m-d\TH:i:s'), $data['rating'], $data['review']);
     print "Ok";
   }
   
@@ -15,7 +15,7 @@ class Rating extends \Fuwafuwa\AjaxController {
     $data = SQL('SELECT c.name, date, rating, review FROM chat_user c 
       JOIN rating r ON c.channel_id = r.channel_id
       WHERE department_id = ?
-      ORDER BY date DESC', data.department_id);
+      ORDER BY date DESC', $data['department_id']);
     $res = ["reviews" => $data]; 
     print(json_encode($res));
   }
